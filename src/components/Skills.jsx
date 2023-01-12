3dbfb625d1094b742c8357266917308aa78d116e
Chris <cbega1227@gmail.com>
2023-01-11 21:39:28 -0700

    Added additonal styling to Skills component.

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 const styles = {
   skillsSection: {
-    backgroundColor: "green",
-    minHeight: "100vh",
+    minHeight: "86vh",
   },
-  skillItem: {},
+  skillsContainer: {
+    padding: "1rem 0.5rem",
+    display: "flex",
+    gap: "1rem",
+    justifyContent: "space-evenly",
+    flexWrap: "wrap",
+  },
+  skillItem: {
+    width: "120px",
+    height: "120px",
+    borderRadius: "50%",
+    backgroundColor: "var(--dark-bg)",
+    display: "grid",
+    justifyContent: "center",
+    alignContent: "center",
+  },
   h2: {
-    margin: "0",
+    textAlign: "center",
   },
 };
 function Skills() {
@@ -15,16 +29,16 @@ function Skills() {
       <section className="skillsSection" style={styles.skillsSection}>
         <h2 style={styles.h2}>Skills</h2>
         <div style={styles.skillsContainer} className="skillsContainer">
-          <div className="skillItem">Html & Css</div>
-          <div className="skillItem">Mongo DB</div>
-          <div className="skillItem">Mongoose</div>
-          <div className="skillItem">Graph Ql</div>
-          <div className="skillItem">mySQL</div>
-          <div className="skillItem">Sequelize</div>
-          <div className="skillItem">Javascript</div>
-          <div className="skillItem">React</div>
-          <div className="skillItem">Node</div>
-          <div className="skillItem">Express</div>
+          <div style={styles.skillItem}>Html & Css</div>
+          <div style={styles.skillItem}>Mongo DB</div>
+          <div style={styles.skillItem}>Mongoose</div>
+          <div style={styles.skillItem}>Graph Ql</div>
+          <div style={styles.skillItem}>mySQL</div>
+          <div style={styles.skillItem}>Sequelize</div>
+          <div style={styles.skillItem}>Javascript</div>
+          <div style={styles.skillItem}>React</div>
+          <div style={styles.skillItem}>Node</div>
+          <div style={styles.skillItem}>Express</div>
         </div>
       </section>
     </>
