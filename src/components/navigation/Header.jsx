667a470fafeb4632bca11f8b6b0bcb730016671d
Chris <cbega1227@gmail.com>
2023-01-11 18:23:00 -0700

    Created Component & Page file structure. Added Nav & About component.

---
+++ src/components/navigation/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navigation from "./Navigation";
+function Header() {
+  //h1 need to be replaced with img/logo
+  return (
+    <header>
+      <h1>Logo</h1>
+      <Navigation />
+    </header>
+  );
+}
+
+export default Header;
