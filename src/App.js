e814e66b95afb863a8e712f066cb7999263bc637
Chris <cbega1227@gmail.com>
2023-01-12 17:31:29 -0700

    Created Resume component, added content, and styled it.

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
           <Route path="/skills" element={<Skills />}></Route>
           <Route path="/projects" element={<Projects />}></Route>
           <Route path="/contact" element={<Contact />}></Route>
-          {/* <Route path="/resume" element={<Resume />}></Route> */}
+          <Route path="/resume" element={<Resume />}></Route>
         </Routes>
         <Footer />
       </Router>
