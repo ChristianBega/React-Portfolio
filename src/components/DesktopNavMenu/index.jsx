8842220796c95cfe455321fb7186d8dcc1f8f36c
Chris <cbega1227@gmail.com>
2023-01-13 19:39:31 -0700

    Created sepereate component folders for mobile and desktop menu.

--- src/components/DesktopNavMenu/index.jsx
+++ src/components/DesktopNavMenu/index.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function DesktopNavMenu() {
-  return <div>DesktopNavMenu</div>;
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/about">About Me</Link>
+        </li>
+        <li>
+          <Link to="/projects">Projects</Link>
+        </li>
+        <li>
+          <Link to="/skills">Skills</Link>
+        </li>
+        <li>
+          <Link to="/Contact">Contact</Link>
+        </li>
+        <li>
+          <Link to="/Resume">Resume</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default DesktopNavMenu;
