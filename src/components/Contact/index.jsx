2eec30bcace1d3344a89e606f66fbb60760f952f
Chris <cbega1227@gmail.com>
2023-01-14 21:05:36 -0700

    Restyled Resume component using Material UI.

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -53,20 +53,3 @@ function Contact() {
 }
 
 export default Contact;
-{
-  /* <>
-//       <section className="contactSection">
-//        
-//         <form style={styles.formContainer}>
-//           <label for="name">Name:</label>
-//           <input type="text" id="name" name="name" placeholder="Enter name here" />
-//           <label for="email">Email:</label>
-//           <input type="email" id="email" name="email" placeholder="Enter email here" />
-//           <label for="contactMessage">Message:</label>
-//           <textarea name="contactMessage" id="contactMessage" placeholder="Inquiries"></textarea>
-//           <button className="formButton">Submit</button>
-//         </form>
-//         <div className="socialLinkContainer">Socials Here</div>
-//       </section>
-//     </> */
-}
--- src/components/Resume/index.jsx
+++ src/components/Resume/index.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Box, Button, Typography, List, ListItem } from "@mui/material/";
+
 const styles = {
   resumeSection: {
     padding: "1rem",
@@ -10,22 +12,67 @@ const styles = {
 };
 function Resume() {
   return (
-    <>
-      <section className="resumeSection" style={styles.resumeSection}>
-        <button style={styles.resumeButton}>Download Resume</button>
-        <h2>Experience</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, necessitatibus aliquam error nam alias nihil eum dignissimos quas eos sequi
-          assumenda, quasi dolor delectus iste iusto id dolores quis, fugiat harum veniam corporis recusandae maiores. Facere, asperiores earum! Iste
-          maiores, atque eaque quod voluptatibus eius?
-        </p>
-        <h2>Eduction</h2>
-        <p>Denver University / Junior Web Developer </p>
-        <small>Student August 2022 - January 2023, Colorado</small>
-        <p>Community College Of Aurora / Associates of General Studies</p>
-        <small>August 2019 - March 2022, Colorado</small>
-      </section>
-    </>
+    <section className="resumeSection" style={{ paddingInline: "1.3rem" }}>
+      <Box textAlign="center" marginTop="2rem">
+        <Button variant="contained" style={{ zIndex: "-1000" }}>
+          Download Resume
+        </Button>
+      </Box>
+      <Box marginTop="2rem">
+        <Typography variant="h4" component="h1" marginBottom="1rem">
+          Experience
+        </Typography>
+        <Box>
+          <Typography variant="h6" component="p" style={{ color: "#fff" }}>
+            Project #1
+          </Typography>
+          <List style={{ color: "var(--medium-gray)" }}>
+            <Typography variant="" component="li" marginBottom="1rem" marginLeft=".6rem">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit corporis commodi veritatis corrupti minima maxime.
+            </Typography>
+            <Typography variant="" component="li" marginBottom="1rem" marginLeft=".6rem">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit corporis commodi veritatis corrupti minima maxime.
+            </Typography>
+            <Typography variant="" component="li" marginBottom="1rem" marginLeft=".6rem">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit corporis commodi veritatis corrupti minima maxime.
+            </Typography>
+          </List>
+        </Box>
+        <Box>
+          <Typography variant="h6" component="p" style={{ color: "#fff" }}>
+            Project #2
+          </Typography>
+          <List style={{ color: "var(--medium-gray)" }}>
+            <Typography variant="" component="li" marginBottom="1rem" marginLeft=".6rem">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit corporis commodi veritatis corrupti minima maxime.
+            </Typography>
+            <Typography variant="" component="li" marginBottom="1rem" marginLeft=".6rem">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit corporis commodi veritatis corrupti minima maxime.
+            </Typography>
+            <Typography variant="" component="li" marginBottom="1rem" marginLeft=".6rem">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit corporis commodi veritatis corrupti minima maxime.
+            </Typography>
+          </List>
+        </Box>
+      </Box>
+      <Box marginTop="2rem">
+        <Typography variant="h4" component="h1" marginBottom="1rem">
+          Eduction
+        </Typography>
+        <Typography variant="" component="p">
+          Denver University / Junior Web Developer
+        </Typography>
+        <Typography variant="" component="small">
+          Student August 2022 - January 2023, Colorado
+        </Typography>
+        <Typography variant="" component="p" marginTop="2rem">
+          Community College Of Aurora / A.G.S
+        </Typography>
+        <Typography variant="" component="small">
+          August 2019 - March 2022, Colorado
+        </Typography>
+      </Box>
+    </section>
   );
 }
 
