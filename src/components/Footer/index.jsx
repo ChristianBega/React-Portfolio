8358898ad7c964b788a5a0bef61fbf407e013db5
Chris <cbega1227@gmail.com>
2023-02-15 12:55:23 -0700

    Refactored footer component to responive grid.

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,28 +1,33 @@
 import { Link } from "react-router-dom";
 import styled from "@emotion/styled";
-import BottomNavigation from "@mui/material/BottomNavigation";
-import { Typography } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import React from "react";
 import Socials from "../Socials";
 
-const StyledBottomNavigation = styled(BottomNavigation)({
+const StyledGrid = styled(Grid)({
   backgroundColor: "var(--dark-header-bg)",
-  display: "flex",
-  justifyContent: "space-between",
+  padding: "1rem 2rem",
   alignItems: "center",
-  padding: ".5rem 2rem",
+  rowGap: "1rem",
 });
-function Footer({ checked }) {
+
+function Footer() {
   return (
-    <>
-      {/* Wrap within AppBar or toolbar to add paper elevation */}
-      <StyledBottomNavigation>
-        <Typography variant="h6" component="h1">
-          <Link to="/React-Portfolio">Chris Bega</Link>
+    <StyledGrid container spacing={2}>
+      <Grid xs={12} md={4}>
+        <Typography variant="h6" component="h1" textAlign="center">
+          <Link to="/">Chris Bega</Link>
         </Typography>
+      </Grid>
+      <Grid xs={6} md={4}>
+        <Typography variant="h6" component="h1" textAlign="center">
+          <Link to="/contact">Contact me</Link>
+        </Typography>
+      </Grid>
+      <Grid xs={6} md={4}>
         <Socials checked={true} />
-      </StyledBottomNavigation>
-    </>
+      </Grid>
+    </StyledGrid>
   );
 }
 
