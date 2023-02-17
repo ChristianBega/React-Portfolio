360eb6236f7be838afcd39f81493eb66abbbec23
Chris <cbega1227@gmail.com>
2023-02-16 18:24:26 -0700

    Finished card styling & and card page.

--- src/pages/ProjectCardsPage.jsx
+++ src/pages/ProjectCardsPage.jsx
@@ -1,13 +1,14 @@
 import { Box, Button, Container } from "@mui/material";
 import React, { useCallback, useState, useEffect } from "react";
 import FinancialTrackerVideo from "../Assets/videos/FinicalAppDemo.mp4";
-import StudyAppImg from "../Assets/images/StudyBuddyApp.png";
+// import StudyAppImg from "../Assets/videos/StudyBuddyApp.png";
 import WeatherDashboardVideo from "../Assets/videos/WeatherDashboardDemo-Mobile.mp4";
 import HangryVideo from "../Assets/videos/HangryDemo.mp4";
 
 import ProjectCard from "../components/ProjectCards";
 import { AiOutlineArrowRight } from "react-icons/ai";
 import { useNavigate } from "react-router";
+import { Link } from "react-router-dom";
 
 export default function ProjectCardsPage() {
   const navigate = useNavigate();
@@ -17,31 +18,41 @@ export default function ProjectCardsPage() {
     {
       name: "React Weather Dashboard",
       description: "A weather dashboard utilizing Open weather api to display daily weather forecast.",
-      image: WeatherDashboardVideo,
+      longDescription: "A long description explaining the process etc...",
+      videoDemo: WeatherDashboardVideo,
       link: "https://reactweatherdashboard.netlify.app/",
       repo: "https://github.com/ChristianBega/Weather-Dashboard",
+      technology: ["React", "Javascript", "3rd Party API", "Tailwind"],
+      // versionHistory : [{}] - pull in github api data to show commit history to show recent improvements.
+      // more about the project - wire frames, figma designs, etc.
     },
     {
       name: "Financial Tracker",
       description: "A MERN full stack application that allows users track bills, savings, and expenses.",
-      image: FinancialTrackerVideo,
+      longDescription: "A long description explaining the process etc...",
+      videoDemo: FinancialTrackerVideo,
       link: "https://golden-goose-finance.herokuapp.com/",
       repo: "https://github.com/ChristianBega/Golden-Goose-Finance",
+      technology: ["React", "Javascript", "MongoDB", "Express", "Node"],
     },
 
     {
       name: "Study Buddies",
       description: "A full stack application that serves as a study productivity aid.",
-      image: StudyAppImg,
+      longDescription: "A long description explaining the process etc...",
+      // videoDemo: StudyAppImg,
       link: "https://studybuddiezzz.herokuapp.com/",
       repo: "https://github.com/JerimiahK/studyBuddies",
+      technology: ["React", "Javascript", "SQL", "Express", "Node", "SCSS", "Material UI"],
     },
     {
       name: "Hangry",
       description: "A food recipe application that provides users with recipes personalized by their dietary and nutritional search filters. ",
-      image: HangryVideo,
+      longDescription: "A long description explaining the process etc...",
+      videoDemo: HangryVideo,
       link: "https://mandiebot.github.io/Hangry/",
       repo: "https://github.com/ChristianBega/Hangry",
+      technology: ["Javascript", "3rd Party API", "Boot Strap"],
     },
   ]);
   useEffect(() => {
@@ -52,7 +63,9 @@ export default function ProjectCardsPage() {
       {/* <Projects /> */}
       {/* map over all project and create cards */}
       {projects.map((project, i) => (
-        <ProjectCard project={project} key={"project" + i} />
+        <Link id={project.name} to="/project-page" state={{ project: project }}>
+          <ProjectCard project={project} key={"project" + i} />
+        </Link>
       ))}
       <Box textAlign="center" marginY={0}>
         <Button onClick={handleOnClick}>
