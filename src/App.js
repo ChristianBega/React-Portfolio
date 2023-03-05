d8c0d6f26916fdc2ca09b780b1ba66b492f058f8
Chris <cbega1227@gmail.com>
2023-03-04 19:40:23 -0700

    Installed framer motion and created animation routes for pages.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import "./index.css";
 import Navigation from "./components/Navigation";
-import HomePage from "./pages/HomePage";
-import ProjectCardsPage from "./pages/ProjectCardsPage";
-import ProjectPage from "./pages/ProjectPage";
-
-import ResumePage from "./pages/ResumePage";
-import ContactPage from "./pages/ContactPage";
+import AnimationRoutes from "./components/AnimationRoutes/AnimationRoutes";
 
 function App() {
   return (
     <>
       <Router>
         <Navigation />
-        <Routes>
-          <Route path="/" element={<HomePage />}></Route>
-          <Route path="/project-cards" element={<ProjectCardsPage />}></Route>
-          <Route path="/project-page" element={<ProjectPage />}></Route>
-          <Route path="/resume" element={<ResumePage />}></Route>
-          <Route path="/contact" element={<ContactPage />}></Route>
-        </Routes>
+        <AnimationRoutes />
       </Router>
     </>
   );
